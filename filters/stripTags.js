d2bc100f44a70a5fd9f94c363982adf28cc01efe
p.pulin <p.pulin@semrush.com>
2015-09-08 13:48:29 +0300

    striptags array

--- filters/stripTags.js
+++ filters/stripTags.js
@@ -3,7 +3,16 @@ var striptags = require('striptags');
 module.exports = function() {
     return function(value, nextFilter) {
         value = value || '';
-        var newValue = striptags(value);
+        var newValue = null;
+
+        if (typeof value === 'object') {
+            newValue = value;
+            for (var name in value) {
+                newValue[name] = striptags(newValue[name])
+            }
+        } else {
+            newValue = striptags(value);
+        }
 
         nextFilter(newValue);
     }
